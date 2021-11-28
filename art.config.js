ad2193cde1659e4b7eed847851891005c56cc9d1
Thomas Ricci <git@thomasricci.dev>
2021-11-28 02:49:12 -0500

    intro sequence!

---
+++ art.config.js
@@ -0,0 +1,32 @@
+export const art = [
+  {
+    title: "This is a long title, hopefully long enough to show what happens when it goes off the page.",
+    description: "This is a long description, hopefully long enough to show what happens when it goes off the page.",
+    imageUrls: [
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720"
+    ]
+  },
+  {
+    title: "foo",
+    description: "bar baz",
+    imageUrls: [
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720"
+    ]
+  },
+  {
+    title: "foo",
+    description: "bar baz",
+    imageUrls: [
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720",
+      "https://picsum.photos/1080/720"
+    ]
+  }
+]
--- main.js
+++ main.js
@@ -20,4 +20,41 @@ const triggerModal = () => {
   }
 }
 
+const introSequence = () => {
+  const ani0 = document.getElementById("animate__intro-0");
+  const ani1 = document.getElementById("animate__intro-1");
+  const ani2 = document.getElementById("animate__intro-2");
+  const ani3 = document.getElementById("animate__intro-3");
+  const ani4 = document.getElementById("animate__intro-4");
+  const ani5 = document.getElementById("animate__intro-5");
+  const root = document.getElementById("root");
+  ani0.addEventListener("animationend", () => {
+    ani1.classList.remove("invisible");
+    ani1.classList.add("animate__fadeIn");
+    setTimeout(() => {
+      ani2.classList.remove("invisible");
+      ani2.classList.add("animate__fadeIn");
+    }, 250);
+  });
+  ani1.addEventListener("animationend", () => {
+    ani3.classList.remove("invisible");
+    ani3.classList.add("animate__fadeInLeft");
+    ani4.classList.add("sona-img-fade-in");
+  });
+  ani3.addEventListener("animationend", () => {
+    ani5.classList.remove("invisible");
+    ani5.classList.add("animate__fadeInDown");
+    setTimeout(() => {
+      const root = document.getElementById("root");
+      const event = new CustomEvent("renderart");
+      root.dispatchEvent(event);
+    }, 500);
+  });
+}
+
+const fadeInImage = (img) => {
+  img.classList.add("piece-img-loaded");
+}
+
+introSequence();
 updateName();
